cb6a095b779c511cd805a9780de08fc5c476fdb5
SanamKhudoyberdieva <khudoyberdievasanam@gmail.com>
2024-02-12 17:33:36 +0500

    news section

--- js/app.js
+++ js/app.js
@@ -57,3 +57,29 @@ $(".lc-clients-slick").slick({
     }
   ]
 });
+
+$(".lc-news-slick").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2
+      }
+    }
+  ]
+});
